2652f7aaa6009b96481bd611bfcb4d0b2ddc32ae
Ali Davoudi <83768943+Ali-Davoudi@users.noreply.github.com>
2023-05-02 20:03:54 +0330

    Update signals.py

--- basket_order_module/signals.py
+++ basket_order_module/signals.py
@@ -33,21 +33,3 @@ def delete_outdate_basket(sender, instance, **kwargs):
 
     t = Timer(30, update_field)  # 86400 seconds = 1440 minutes = 24 hours
     t.start()
-
-# @receiver(post_save, sender=UserBasketOrder)
-# def delete_old_basket_orders(sender, instance, **kwargs):
-#     if instance.is_paid is False and instance.created_at < timezone.now() - timezone.timedelta(minutes=30):
-#         instance.userbasketorderdetail_set.all().delete()
-#         instance.delete()
-#
-#
-# # Get all orders with is_paid as False and older than 30 minutes
-# old_orders = UserBasketOrder.objects.filter(is_paid=False,
-#                                             created_at__lte=timezone.now() - timezone.timedelta(
-#                                                 minutes=3))
-# # Loop through each old order and delete its order details
-# for old_order in old_orders:
-#     old_order.userbasketorderdetail_set.all().delete()
-#
-# # Delete the old orders
-# old_orders.delete()
